491c0fd701c05a9191e296f88612ed1398881717
Jesus Renteria <Jesus Renteria>
2019-11-11 12:41:48 -0600

    added request variables, and switch inputs

--- node.js
+++ node.js
@@ -1,1 +1,31 @@
-// http://www.omdbapi.com/?apikey=[yourkey]&
+var request = require("request");
+var moment = require("moment");
+var spotify = require("node-spotify-api");
+var fs = require("fs");
+
+var keys = require(".//keys");
+let liriInput = process.argv[2];
+let input = process.argv.slice(3).join(" ");
+
+switch (liriInput){
+    case "concert-this":
+        concertThis(input);
+        break;
+    case "spotify-this-song":
+        spotifyThisSong(input);
+        break;
+    case "movie-this":
+        if(input == null){
+            movieThis("The Terminator");
+        }else{
+            movieThis(input);
+        }
+        break;
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
+        default:
+        console.log('--------------Please use one of the folloring four Liri functions:1.concert-this 2.spotify-this-song 3.movie-this 4. do-what-it-says-------------');
+}
+
+//concert this
